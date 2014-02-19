cd0ce1ed59c1b8465fb4ab7e1254c7176e462a56
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2014-02-19 14:52:07 -0500

    Worked on third function, still have to plot img

--- hw4/random_art.py
+++ hw4/random_art.py
@@ -45,16 +45,42 @@ def evaluate_random_function(f, x, y):
         return evaluate_random_function(f[1], x, y) * evaluate_random_function(f[2], x, y)
     elif f[0] == "avg":
         return (evaluate_random_function(f[1], x, y) + evaluate_random_function(f[2], x, y))/2.0
-    
-#def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
+
+def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
     """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
         to the output interval [output_interval_start, output_interval_end].  The mapping
         is an affine one (i.e. output = input*c + b).
     
         TODO: please fill out the rest of this docstring
     """
-    # your code goes here
-
+    num = float((output_interval_end - output_interval_start)*(val - input_interval_start))
+    denom = input_interval_end - input_interval_start    
+    res = num/denom + output_interval_start    
+    return res    
+    
+def create_image():
+    red_ch = build_random_function(1,3)
+    green_ch = build_random_function(1,3)
+    blue_ch = build_random_function(1,3)
+    
+    im = Image.new("RGB", (350,350))
+    pixels = im.load()
+    
+    for x in range(350):
+        x_val = remap_interval(x, 0, 349, -1, 1)
+        for y in range(350):
+            y_val = remap_interval(y, 0, 349, -1, 1)
+            red_val = evaluate_random_function(red_ch, x_val, y_val)
+            green_val = evaluate_random_function(green_ch, x_val, y_val)
+            blue_val = evaluate_random_function(blue_ch, x_val, y_val)
+            red = remap_interval(red_val, -1, 1, 0, 255)
+            green = remap_interval(green_val, -1, 1, 0, 255)
+            blue = remap_interval(blue_val, -1, 1, 0, 255)
+            pixels[int(x),int(y)] = (int(red), int(green), int(blue))
+    im.show()
+            
 if __name__ == '__main__':
     print f
     print evaluate_random_function(f,5,5)
+    print remap_interval(75, 50, 100, 0, 1)
+    create_image()
