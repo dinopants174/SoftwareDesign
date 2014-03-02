1d3ca4d125cdb257ae5e63ab769af95b394cf607
unknown <nickfrancisci@gmail.com>
2014-03-02 14:58:14 -0500

    grading

--- hw3/gene_finder.py
+++ hw3/gene_finder.py
@@ -9,7 +9,7 @@ from amino_acids import aa, codons #used to convert nucleotides to amino acids
 from random import shuffle #used to shuffle dna for trials
 
 from load import load_seq #used to load Salmonella DNA
-dna = load_seq("./data/X73525.fa")
+#dna = load_seq("./data/X73525.fa")
 
 #I know I should be using "if _name_ = main" thing somewhere but I don't really know how it works
 
@@ -32,6 +32,9 @@ def coding_strand_to_AA(dna):
     AA = ""
     for i in range(len(dna)):
         codon = dna[3*i:3*i+3]  #breaks up DNA into sequences of 3 nucleotides = codon
+        						# This is one possible solution to incrementing by 3.You
+        						# can also assign 3 arguments to range() so that it goes
+        						# range(start, stop, increment)
         for t in range(len(codons)):
             if codon in codons[t]:
                 AA += aa[t] #loops through codon list, checks to see if codon found is in list, adds amino acid of corresponding index
@@ -62,7 +65,10 @@ def get_reverse_complement(dna):
             reverse_dna += "C"
         else:   #if it finds a letter that doesn't have a complement, it prints that there is an error in the DNA
             return "I have recieved a character that is not defined in this loop"
+            		#Hmm. Interesting. Obvously it should never reach this case, but accounting for potential
+            		# errors is never a bad thing, so well done.
     return reverse_dna[::-1]
+    				#Good use of the substring syntax to reverse this list
     
 def get_reverse_complement_unit_tests():
     """ Unit tests for the get_complement function """
@@ -82,9 +88,13 @@ def rest_of_ORF(dna):
     for i in range(0,len(dna),3):
         codon = dna[i:i+3] #breaks up DNA into codons
         if codon != 'TAG' and codon != 'TAA' and codon != 'TGA':
+        						#Yep, another possible option here is "if not codon in ['TAG','TAA', 'TGA']:"
             orf += codon #concatenates codons in string unless it reaches a stop codon
         else:
             break #at stop codon, function ends, possibly not the most elegant solution
+            		# I disagree, this is quite a succinct way to handle the problem of needing
+            		# to return if you find a stop codon, but also handle the case of returning
+            		# the entire string if you don't encounter one. Good job.
     return orf
 
 def rest_of_ORF_unit_tests():
@@ -131,11 +141,17 @@ def find_all_ORFs(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
+
+    # This function is bugged! My unit tests show the below:
+    # input: ATGCATGAATGTAG, expected output: ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG'] , actual output: ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
+	# input: ATGTGCATGAGATAGGATGGGATGCTTG, expected output: ['ATGTGCATGAGA', 'ATGCTTG', 'ATGGGATGCTTG'], actual output: ['ATGTGCATGAGAATGCTTG', 'ATGGGATGCTTG', '']
+
     p = 0
     res = []
     while p<3: #starts reading frame at index[0] => index[1] => index[2] b/c index[3] = index[0]
         snip = dna[p:] #creates a snippet of DNA that extends from starting index to end of DNA
-        res.append(collapse(find_all_ORFs_oneframe(snip)))
+        res.append(collapse(find_all_ORFs_oneframe(snip)))	#This is your problem, each time a list returns, you're collapsing it into one
+        													# long string before you append it to your list.
         p += 1
     return res
 
@@ -152,11 +168,20 @@ def find_all_ORFs_both_strands(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     """
+
+    # This function is bugged too! In addition to any cascading effects from the bug in the previous function,
+    # you make the same syntactical mistake here too. (Its alright, I'll only count it once, but write some
+    # more robust testing functions in future!)
+	# input: ATGCGAATGTAGCATCAAA, expected output: ['ATGCGAATG', 'ATGCTACATTCGCAT'] , actual output: ['ATGCGAATG', 'ATGCTACATTCGCAT']
+	# input: ATGTGCATGAGATAGGATGGGATGCTTG, expected output: ['ATGTGCATGAGA', 'ATGCTTG', 'ATGGGATGCTTG', 'ATGCACAT'], 
+	# actual output: ['ATGTGCATGAGAATGCTTGATGGGATGCTTG', 'ATGCACAT']
+
     res = []
     dna_rev = get_reverse_complement(dna)
     orf = find_all_ORFs(dna)
     orf_rev = find_all_ORFs(dna_rev)
     res.append(collapse(orf)) #appends to an empty list the reading frames from original strand of DNA
+    							#This syntax is generating the bugs just like above.
     res.append(collapse(orf_rev)) #appends to list the reading frames from complementary strand of DNA
     return res
     
@@ -172,6 +197,8 @@ def longest_ORF(dna):
         as a string"""
     t = find_all_ORFs_both_strands(dna) #stores all of the reading frames in t
     return max(t, key=len)
+    	# Good use of max. I would add that you don't need to define your variable as t first - you could one-line this function -
+    	# but thats a matter of personal taste
 
 def longest_ORF_unit_tests():
     """ Unit tests for the longest_ORF function """
@@ -195,7 +222,9 @@ def longest_ORF_noncoding(dna, num_trials):
             longest_frame = len(longest_ORF(dna_string)) #keeps replacing if it finds longer reading frame until found max
     return longest_frame
 
-print (longest_ORF_noncoding(dna, 1500))/9 #so something weird is happening here, was getting thresholds in 6000s, divided by 9 to get realistic threshold
+#print (longest_ORF_noncoding(dna, 1500))/9 #so something weird is happening here, was getting thresholds in 6000s, divided by 9 to get realistic threshold
+# I assume this is linked to the fact that you are appending all of your strings together above. I don't know for certain because this function does
+# have some randomness in my behavior, but if you debug above, it will probably sort this out.
 
 def gene_finder(dna, threshold):
     """ Returns the amino acid sequences coded by all genes that have an ORF
@@ -218,4 +247,4 @@ def gene_finder(dna, threshold):
             i += 1
     return res
 
-print gene_finder(dna,750)
+#print gene_finder(dna,750)
